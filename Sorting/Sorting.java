1bf04097772ddf0c238046a8417f232e11024735
Suryanmishra762 <suryanarayanmishra762@gmail.com>
2026-02-03 20:39:06 +0530

    Added Sorting Techniques

---
+++ Sorting/Sorting.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Sorting {
+
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {7,8,3,1,2};
+
+        //bubble sort
+        //time complexity = O(n^2)
+        // for(int i = 0; i < arr.length-1; i++){
+        //     for(int j = 0; j <arr.length-i-1; j++){
+        //         if(arr[j] > arr[j+1]){
+        //             //swap
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //         }
+        //     }
+        // }
+
+        // printArr(arr);
+
+        //selection Sort
+        //time complexity = O(n^2)
+        // for(int i = 0; i < arr.length-1 ; i++){
+        //     int smallest = i;
+        //     for(int j = i+1; j < arr.length ; j++){
+        //         if(arr[smallest] > arr[j]){
+        //             smallest = j;
+        //         }
+        //     }
+        //     int temp = arr[i];
+        //     arr[i]= arr[smallest];
+        //     arr[smallest]= temp ;
+        //     // int temp = arr[smallest];
+        //     // arr[smallest] = arr[i];
+        //     // arr[i] = temp;
+        // }
+
+        // printArr(arr);
+        
+        //Insertion Sort
+        //time complexity = O(n^2)
+        for(int i = 1; i < arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j >=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            //placement
+            arr[j+1] = current;
+        }
+        printArr(arr);
+    }
+
+    
+}
